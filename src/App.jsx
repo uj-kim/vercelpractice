625444cb2957376eb361aef10334695b31129aad
uj-kim <uj-kim@users.noreply.github.com>
2024-11-18 07:24:01 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+    const [count, setCount] = useState(0);
+    const [value, setValue] = useState('');
+    const handleCountPlus = () => {
+        setCount(count + 1);
+    };
+
+    useEffect(() => {
+        console.log('hello useEffect');
+    });
+
+    return (
+        <>
+            {count}
+            <br />
+            <button onClick={handleCountPlus}>증가</button>
+            <h1>useEffect</h1>
+            <input
+                type="text"
+                value={value}
+                onChange={(e) => {
+                    setValue(e.target.value);
+                }}
+            />
+        </>
+    );
+}
+
+export default App;
